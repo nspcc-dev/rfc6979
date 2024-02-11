0f0c5a3ea62947db87cd0fc91229a733879735f0
Roman Khimov <roman@nspcc.ru>
2024-02-11 17:50:02 +0300

    bench: add some trivial, though typical benchmark
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

---
+++ bench_test.go
@@ -0,0 +1,18 @@
+package rfc6979_test
+
+import (
+	"crypto/sha256"
+	"testing"
+
+	"github.com/nspcc-dev/rfc6979"
+)
+
+func BenchmarkECDSASign(b *testing.B) {
+	const msg = "Hello world!"
+
+	h := sha256.Sum256([]byte(msg))
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_, _ = rfc6979.SignECDSA(p256.key, h[:], sha256.New)
+	}
+}
